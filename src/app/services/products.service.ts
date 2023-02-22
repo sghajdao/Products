226daa75cee6cf0c8eae53ff11ad19c68cd3b10c
sghajdao <saadghajdaoui@gmail.com>
2023-02-22 15:04:30 +0100

    NgRx

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -11,41 +11,32 @@ export class productsService {
     }
 
     getAllProducts():Observable<Product[]> {
-        let host=environment.host;
-        return this.http.get<Product[]>(host+"/products");
+        return this.http.get<Product[]>("http://localhost:3000/products");
     }
     getSelectedProducts():Observable<Product[]> {
-        let host=environment.host;
-        return this.http.get<Product[]>(host+"/products?selected=true");
+        return this.http.get<Product[]>("http://localhost:3000/products?selected=true");
     }
     getAvailableProducts():Observable<Product[]> {
-        let host=environment.host;
-        return this.http.get<Product[]>(host+"/products?available=true");
+        return this.http.get<Product[]>("http://localhost:3000/products?available=true");
     }
     searchProducts(keyword:string):Observable<Product[]> {
-        let host=environment.host;
-        return this.http.get<Product[]>(host+"/products?name_like="+keyword);
+        return this.http.get<Product[]>("http://localhost:3000/products?name_like="+keyword);
     }
     select(product:Product):Observable<Product> {
-        let host=environment.host;
         product.selected=!product.selected;
-        return this.http.put<Product>(host+"/products/"+product.id, product);
+        return this.http.put<Product>("http://localhost:3000/products/"+product.id, product);
     }
     delete(product:Product):Observable<void> {
-        let host=environment.host;
         product.selected=!product.selected;
-        return this.http.delete<void>(host+"/products/"+product.id);
+        return this.http.delete<void>("http://localhost:3000/products/"+product.id);
     }
     save(product:Product):Observable<Product> {
-        let host=environment.host;
-        return this.http.post<Product>(host+"/products/", product);
+        return this.http.post<Product>("http://localhost:3000/products/", product);
     }
     getProducts(id:number):Observable<Product> {
-        let host=environment.host;
-        return this.http.get<Product>(host+"/products/"+id);
+        return this.http.get<Product>("http://localhost:3000/products/"+id);
     }
     UpdateProducts(product:Product):Observable<Product> {
-        let host=environment.host;
-        return this.http.put<Product>(host+"/products/"+product.id, product);
+        return this.http.put<Product>("http://localhost:3000/products/"+product.id, product);
     }
 }
